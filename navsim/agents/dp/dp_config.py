74b96f9efc3b6b63f68ad7b6fe8135ac53ef98cb
EdmChao <echao64@att.net>
2026-02-24 21:39:03 -0800

    revert to denoise

--- navsim/agents/dp/dp_config.py
+++ navsim/agents/dp/dp_config.py
@@ -47,7 +47,7 @@ class DPConfig(TransfuserConfig):
     #  - sampling_noise_scale: multiply initial Gaussian noise ( >1 => more diverse)
     #  - sampling_inference_steps: if >0, override the number of denoising steps used at inference
     sampling_noise_scale: float = 1.5
-    sampling_inference_steps: int = -1
+    sampling_inference_steps: int = 0
 
     seq_len: int = 2
     trajectory_imi_weight: float = 1.0
